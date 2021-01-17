3b7fe23d04afff5f3c63ef87cd4d1192eb1918ca
raulmyocu <rauloctaviomurcia@gmail.com>
2021-01-17 13:57:39 -0700

    graph functions style

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,18 +1,18 @@
+/*
+Filename: linkedlist.cpp
+Authors: Domínguez Acosta María Fernanda
+         Murcia Yocupicio Raúl Octavio
+         Sandoval del Hoyo María Ximena
+Date: 17/01/2021
+Description: Functions of the implementetion of a graph
+*/
+#include <iostream>
 #include "graph.h"
 
-#include <iostream>
-
-Graph::Graph() {
-    ;
-}
-
-Graph::~Graph() {
-    //to do
-    ;
-}
+//**********************************************************************//
 
 bool Graph::add_node(string tag) {
-    if (!find_node(tag)) {
+    if (!find_node(tag)) { // If tag found
         GraphNode new_node;
         new_node.tag = tag;
         nodes.add(new_node);
@@ -21,11 +21,14 @@ bool Graph::add_node(string tag) {
     return false
 }
 
+//**********************************************************************//
+
 bool Graph::add_edge(string node1_tag, string node2_tag, string edge_tag) {
+    //find both nodes
     GraphNode* p1 = find_node(node1_tag);
     GraphNode* p2 = find_node(node2_tag);
     if (!find_edge(edge_tag) && p1 && p2) {
-        GraphEdge new_edge1;
+        GraphEdge new_edge1; // Link them using the graph edge as value
         new_edge.tag = edge_tag;
         new_edge.node = p2;
         p1->edges.add(new_edge);
@@ -36,13 +39,19 @@ bool Graph::add_edge(string node1_tag, string node2_tag, string edge_tag) {
     return false
 }
 
+//**********************************************************************//
+
 bool Graph::remove_node(string tag) {
     GraphNode* p = find_node(tag);
+    GraphEdge tmp_edge; // pivot edge to delete edges
     for (GraphEdge edge: p->edges) {
-        edge.node.edges.remove(*p)
+        tmp_edge.tag = edge.tag // assign the tag to delete from edges
+        edge.node->edges.remove(tmp_edge)
     }
     nodes.remove(*p)
 }
+
+//**********************************************************************//
 
 bool Graph::remove_edge(string node1_tag, string node2_tag) {
     GraphNode* p1 = find_node(node1_tag);
@@ -51,6 +60,8 @@ bool Graph::remove_edge(string node1_tag, string node2_tag) {
     p2->edges.remove(*p1)
 }
 
+//**********************************************************************//
+
 bool Graph::remove_edge(string tag) {
     GraphNode* p = find_node(tag);
     for (GraphNode node: nodes) {
@@ -58,11 +69,15 @@ bool Graph::remove_edge(string tag) {
     }
 }
 
+//**********************************************************************//
+
 bool Graph::find_node(string tag) {
     GraphNode temp_node;
     pivot_node.tag = tag;
     return (bool) nodes.find(temp_node);
 }
+
+//**********************************************************************//
 
 GraphNode* Graph::find_node(string tag) {
     GraphNode temp_node;
@@ -71,6 +86,8 @@ GraphNode* Graph::find_node(string tag) {
     if (tmp) return &(tmp->value);
     return nullptr;
 }
+
+//**********************************************************************//
 
 bool Graph::find_edge(string node1_tag, string node2_tag) {
     GraphNode* node1_ptr = find_node(node1_tag);
@@ -81,6 +98,8 @@ bool Graph::find_edge(string node1_tag, string node2_tag) {
     }
     return false;
 }
+
+//**********************************************************************//
 
 bool Graph::find_edge(string tag) {
     GraphEdge pivot_edge;
