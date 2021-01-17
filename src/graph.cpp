44e013bc6d23f03a75741ae95fb2c82da6afe60d
raulmyocu <rauloctaviomurcia@gmail.com>
2021-01-17 16:19:41 -0700

    fixes

--- src/graph.cpp
+++ src/graph.cpp
@@ -8,6 +8,7 @@ Description: Functions of the implementetion of a graph
 */
 #include <iostream>
 #include "graph.h"
+#include "graphstructs.h"
 
 //**********************************************************************//
 
@@ -16,82 +17,100 @@ bool Graph::add_node(string tag) {
         GraphNode new_node;
         new_node.tag = tag;
         nodes.add(new_node);
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 
 //**********************************************************************//
 
 bool Graph::add_edge(string node1_tag, string node2_tag, string edge_tag) {
     //find both nodes
-    GraphNode* p1 = find_node(node1_tag);
-    GraphNode* p2 = find_node(node2_tag);
+    GraphNode* p1 = get_node(node1_tag);
+    GraphNode* p2 = get_node(node2_tag);
     if (!find_edge(edge_tag) && p1 && p2) {
-        GraphEdge new_edge1; // Link them using the graph edge as value
+        GraphEdge new_edge; // Link them using the graph edge as value
         new_edge.tag = edge_tag;
         new_edge.node = p2;
         p1->edges.add(new_edge);
         new_edge.node = p1;
         p2->edges.add(new_edge);
-        return true
+        return true;
     }
-    return false
+    return false;
 }
 
 //**********************************************************************//
 
 bool Graph::remove_node(string tag) {
-    GraphNode* p = find_node(tag);
-    GraphEdge tmp_edge; // pivot edge to delete edges
-    for (GraphEdge edge: p->edges) {
-        tmp_edge.tag = edge.tag // assign the tag to delete from edges
-        edge.node->edges.remove(tmp_edge)
+    GraphNode* p = get_node(tag);
+    if (p) {
+        GraphEdge tmp_edge; // pivot edge to delete edges
+        for (GraphEdge edge: p->edges) {
+            tmp_edge.tag = edge.tag; // assign the tag to delete from edges
+            edge.node->edges.remove(tmp_edge);
+        }
+        nodes.remove(*p);
+        return true;
     }
-    nodes.remove(*p)
+    return false;
 }
 
 //**********************************************************************//
 
 bool Graph::remove_edge(string node1_tag, string node2_tag) {
-    GraphNode* p1 = find_node(node1_tag);
-    GraphNode* p2 = find_node(node2_tag);
-    p1->edges.remove(*p2)
-    p2->edges.remove(*p1)
+    GraphNode* p1 = get_node(node1_tag);
+    GraphNode* p2 = get_node(node2_tag);
+    if (!p1 || !p2) return false;
+    bool found = false;
+    for (GraphEdge edge: p1->edges) {
+        if (edge.node == p2) {
+            GraphEdge tmp_edge;
+            tmp_edge.tag = edge.tag;
+            p1->edges.remove(tmp_edge);
+            p2->edges.remove(tmp_edge);
+            found = true;
+            break;
+        }
+    }
+    return found;
 }
 
 //**********************************************************************//
 
 bool Graph::remove_edge(string tag) {
-    GraphNode* p = find_node(tag);
+    bool deleted = false;
+    GraphEdge tmp_edge;
+    tmp_edge.tag = tag;
     for (GraphNode node: nodes) {
-        node.edges.remove(tag)
+        if (node.edges.remove(tmp_edge)) {
+            deleted = true;
+        }
     }
+    return deleted;
 }
 
 //**********************************************************************//
 
 bool Graph::find_node(string tag) {
-    GraphNode temp_node;
-    pivot_node.tag = tag;
-    return (bool) nodes.find(temp_node);
+    return (bool) get_node(tag);
 }
 
 //**********************************************************************//
 
-GraphNode* Graph::find_node(string tag) {
+GraphNode* Graph::get_node(string tag) {
     GraphNode temp_node;
-    pivot_node.tag = tag
+    temp_node.tag = tag;
     auto list_node = nodes.find(temp_node);
-    if (tmp) return &(tmp->value);
+    if (list_node) return &(list_node->value);
     return nullptr;
 }
 
 //**********************************************************************//
 
 bool Graph::find_edge(string node1_tag, string node2_tag) {
-    GraphNode* node1_ptr = find_node(node1_tag);
-    GraphNode* node2_ptr = find_node(node2_tag);
+    GraphNode* node1_ptr = get_node(node1_tag);
+    GraphNode* node2_ptr = get_node(node2_tag);
 
     for (GraphEdge edge: node1_ptr->edges) {
         if (edge.node == node2_ptr) return true;
@@ -105,7 +124,7 @@ bool Graph::find_edge(string tag) {
     GraphEdge pivot_edge;
     pivot_edge.tag = tag;
     for (GraphNode node: nodes) {
-        if (node.edges.find(pivot_edge)) return true
+        if (node.edges.find(pivot_edge)) return true;
     }
-    return false
+    return false;
 }
