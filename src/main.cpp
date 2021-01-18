710363e4e96a2eca06096421bfa1f0a51b39254c
Fernanda Dominguez <fernanda.dominguezac@gmail.com>
2021-01-17 22:33:16 -0700

    input from file

--- src/main.cpp
+++ src/main.cpp
@@ -13,73 +13,38 @@ Description: Main class of
 
 using std::cout;
 using std::endl;
+using std::ifstream;
 
 int main(){
-   /*
-    // creation ***********************************
-    cout << "creando G" << endl;
-    Graph G;
-    cout << "G creado" << endl;
-    G.print();
-
-    // add vertices ***********************************
-    G.add_node("v1");
-    cout << "v1 creado" << endl;
-    G.print();
-
-    G.add_node("v2");
-    cout << "v2 creado" << endl;
-    G.print();
-
-    G.add_node("v3");
-    cout << "v3 creado" << endl;
-    G.print();
-
-    G.add_node("v4");
-    cout << "v4 creado" << endl;
-    G.print();
-
-    // add edges ***********************************
-    G.add_edge("v1", "v2", "e1");
-    cout << "v12 creado" << endl;
-    G.print();
-
-    G.add_edge("v2", "v3", "e2");
-    cout << "v23 creado" << endl;
-    G.print();
-
-    G.add_edge("v3", "v4", "e3");
-    cout << "v34 creado" << endl;
-    G.print();
-
-    // delete edges ******************************
-    G.remove_edge("e3");
-    cout << "e3 eliminado" << endl;
-    G.print();
-
-    G.remove_edge("v1", "v2");
-    cout << "v1 v2 eliminado" << endl;
-    G.print();
-
-    //delete nodes *******************
-    G.remove_node("v3");
-    cout << "v3 eliminado" << endl;
-    G.print();
-    */
 
     Graph G;
 
-    G.add_node("v1");
-    G.add_node("v2");
-    G.add_edge("v1", "v1", "e1");
-    G.add_edge("v1", "v2", "e2");
+    ifstream file;
+    
+    std::string v="", v1, v2, tag;
+    file.open("graph.txt");
+    if(file.fail()){
+        cout << "Error Opening File... \n";
+        exit(1);
+    }
 
-    G.add_node("v3");
+    file >> v;
+    while(v != "<TOKEN>"){
+       G.add_node(v);
+       file >> v;
+    }
+    while(file.eof() == false){
+       file >> v1 >> v2 >> tag;
+       G.add_edge(v1, v2, tag);
+    }
+    cout << "     **GRAPH**      " << endl;
     G.print();
+    cout << "Nodes: " << G.node_count() << endl;
+    cout << "Edges: " << G.edge_count() << endl;
 
-    G.clear();
-
-    G.print();
+    //G.remove_edges("d");
+    //cout << "rm d: edge with a, c, f" << endl;
+    //G.print();
 
     return 0;
 }
