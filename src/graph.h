70174c75ded0f3c05e8e86b65dee316282a5ce3e
ximenasandoval <ximena.sandoval.dh@gmail.com>
2021-01-17 21:09:53 -0700

    :memo: Adding graph.h documentation

--- src/graph.h
+++ src/graph.h
@@ -27,15 +27,36 @@ public:
 
     GraphNode* get_node(string tag);
 
+    /**
+     * @brief Function that obtains node's degree of given node
+     * @param tag node's tag 
+     **/
     int node_degree(string tag);
 
+    /**
+     *@brief Function that obtains graph's node count
+     **/
     int node_count();
+
+    /**
+     *@brief Function that obtains graph's edge count
+     **/
     int edge_count();
 
+    /**
+     *@brief Function that removes all incising edges of given a node
+     *@param tag node's tag which edges will be removed
+     **/
     bool remove_edges(string tag);
 
+    /**
+     *@brief Function to clear all graph's nodes and edges
+     **/
     void clear();
 
+    /**
+     *@brief Function to print the graph
+     **/
     void print();
 
 };
